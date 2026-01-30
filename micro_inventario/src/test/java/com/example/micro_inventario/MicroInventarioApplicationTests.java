cd6bd594c4a4f6983e9025c590d9303f3173e8ea
kulialma <miguelrums@gmail.com>
2026-01-30 17:14:47 -0500

    ajustes e implementaciones

--- micro_inventario/src/test/java/com/example/micro_inventario/MicroInventarioApplicationTests.java
+++ micro_inventario/src/test/java/com/example/micro_inventario/MicroInventarioApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.micro_inventario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")   // 👈 Forzamos a usar application-test.properties
 class MicroInventarioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Este test solo valida que el contexto de Spring arranca correctamente
+    }
 }
+
